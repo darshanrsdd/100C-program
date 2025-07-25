77c33b4630a424ecc7f8cd872d5230f4e6fce2d5
darshanrsdd <darshanrsdd@gmail.com>
2025-07-25 23:12:56 +0530

    Add files via upload

---
+++ p14.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num, square;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    square = num * num;
+
+    printf("The square of %d is: %d\n", num, square);
+    return 0;
+}
