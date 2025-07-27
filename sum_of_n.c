081cb202188cb918223d6a7b28fae958de0cf161
darshanrsdd <darshanrsdd@gmail.com>
2025-07-27 19:59:51 +0530

    Add files via upload

---
+++ sum_of_n.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+ {
+    int n, product = 1;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        product *= i;
+    }
+    printf("Product = %d", product);
+    return 0;
+}
